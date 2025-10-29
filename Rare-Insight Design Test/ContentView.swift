291deb10f56dc0470821683b5366dabfb5ced545
Heather <sezhzes@gmail.com>
2025-10-29 11:43:34 -0700

    Initial Commit

---
+++ Rare-Insight Design Test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rare-Insight Design Test
+//
+//  Created by Heather Szczesniak on 10/29/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Rare-Insight Design Test/Rare_Insight_Design_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rare_Insight_Design_TestApp.swift
+//  Rare-Insight Design Test
+//
+//  Created by Heather Szczesniak on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Rare_Insight_Design_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
